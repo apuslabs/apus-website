9ae3bf9d4e22081c959938c93cfa131fb9c6a75d
Jax Williams <jax@apus.network>
2024-05-24 10:16:22 +0800

    clean & compress image

--- src/assets/image.ts
+++ src/assets/image.ts
@@ -1,11 +1,7 @@
 export { default as ApusLogo } from './logo-apus.png'
-export { default as AvatarDefault } from './avatar-default.png'
 
 import LogoHorizonal from './homepage/logo_horizonal.png'
 import LogoHorizonalWhite from './homepage/logo_horizonal_white.png'
-import BgHero from './homepage/bg-hero-desktop.png'
-import BgSlogan from './homepage/bg-slogan.png'
-import IconHeroLogo from './homepage/icon-hero-logo.png'
 import IconFeaturesGPUIntegrity from './homepage/features-gpu-integrity.png'
 import IconFeaturesAIModel from './homepage/features-ai-model.png'
 import IconFeaturesVerifiableResult from './homepage/features-result.png'
@@ -18,43 +14,26 @@ import LogoArweave from './homepage/logo-arweave.png'
 import LogoPPIO from './homepage/logo-ppio.png'
 import LogoAO from './homepage/logo-ao.png'
 import LogoDephy from './homepage/logo-dephy.png'
-import LogoLagrange from './homepage/logo-lagrange.png'
 import LogoNovita from './homepage/logo-novita.png'
-import LogoGpunet from './homepage/logo-gpunet.png'
-import LogoZkpool from './homepage/logo-zkpool.png'
-import LogoW3bstream from './homepage/logo-w3bstream.png'
-import LogoTaiko from './homepage/logo-taiko.png'
-import LogoScroll from './homepage/logo-scroll.png'
-import LogoEigenlayer from './homepage/logo-eigenlayer.png'
 import IconMenu from './homepage/icon-menu.png'
 import BgSolution from './homepage/solution.png'
 
 export const ImgHomepage = {
   LogoHorizonal,
   LogoHorizonalWhite,
-  BgHero,
-  BgSlogan,
-  IconHeroLogo,
   BgHighlights,
   BgHighlightsMobile,
   BgSolution,
   IconFeaturesGPUIntegrity,
   IconFeaturesAIModel,
   IconFeaturesVerifiableResult,
-  LogoAO,
   IconArrowRight,
   LogoAbyssWorld,
   LogoSolana,
   LogoArweave,
+  LogoAO,
   LogoPPIO,
   LogoDephy,
-  LogoLagrange,
   LogoNovita,
-  LogoGpunet,
-  LogoZkpool,
-  LogoW3bstream,
-  LogoTaiko,
-  LogoScroll,
-  LogoEigenlayer,
   IconMenu
 }
