02dfa6d3d2f7a2861268a5c4beeff34c50aae2da
Jax Williams <jax@apus.network>
2024-06-18 14:41:41 +0800

    remove unused img

--- src/assets/image.ts
+++ src/assets/image.ts
@@ -2,19 +2,9 @@ export { default as ApusLogo } from './logo-apus.png'
 
 import LogoHorizonal from './homepage/logo_horizonal.png'
 import LogoHorizonalWhite from './homepage/logo_horizonal_white.png'
-import IconFeaturesGPUIntegrity from './homepage/features-gpu-integrity.png'
-import IconFeaturesAIModel from './homepage/features-ai-model.png'
-import IconFeaturesVerifiableResult from './homepage/features-result.png'
-import BgHighlights from './homepage/earth.png'
-import BgHighlightsMobile from './homepage/earth-m.png'
-import IconArrowRight from './homepage/icon-arrow-right.png'
-import LogoAbyssWorld from './homepage/logo-abyssworld.png'
-import LogoSolana from './homepage/logo-solana.png'
 import LogoArweave from './homepage/logo-arweave.png'
-import LogoPPIO from './homepage/logo-ppio.png'
 import LogoAO from './homepage/logo-ao.png'
 import LogoDephy from './homepage/logo-dephy.png'
-import LogoNovita from './homepage/logo-novita.png'
 import IconMenu from './homepage/icon-menu.png'
 import BgSolution from './homepage/solution.png'
 import BgHero from './homepage2/bg_kv.png'
@@ -37,22 +27,12 @@ import IconAnnouncement from './homepage2/icon-announcement.png'
 export const ImgHomepage = {
   LogoHorizonal,
   LogoHorizonalWhite,
-  BgHighlights,
-  BgHighlightsMobile,
-  BgSolution,
-  IconFeaturesGPUIntegrity,
-  IconFeaturesAIModel,
-  IconFeaturesVerifiableResult,
-  IconArrowRight,
-  LogoAbyssWorld,
-  LogoSolana,
   LogoArweave,
   LogoAO,
-  LogoPPIO,
   LogoDephy,
-  LogoNovita,
   IconMenu,
   BgHero,
+  BgSolution,
   FeaturesCost,
   FeaturesExecution,
   FeaturesIncentive,
