7138c17a78de3db52011ee9c0b50dfb510ddc3e2
Jax Williams <jax@apus.network>
2024-06-30 10:13:04 +0800

    ignore build wanring

--- src/pages/HomeIndex/index.tsx
+++ src/pages/HomeIndex/index.tsx
@@ -357,4 +357,4 @@ const HomeIndex: FC = () => {
 export default HomeIndex;
 
 
-const TwitterVideo = () => <iframe width="560" height="315" src="https://www.youtube.com/embed/-rPbCeCbJVc?si=778KTA2eKneoQLDF" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+const TwitterVideo = () => <iframe width="560" height="315" src="https://www.youtube.com/embed/-rPbCeCbJVc?si=778KTA2eKneoQLDF" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
