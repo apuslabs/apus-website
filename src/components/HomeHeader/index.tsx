10ecdc64da039d976d11114b217dcfdfe730b33f
Jax Williams <jax@apus.network>
2024-08-09 12:43:43 +0800

    add console to homepage

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -3,6 +3,7 @@ import { ImgHomepage } from "../../assets/image";
 import { useNavigate, Link } from "react-router-dom";
 import { useBreakpoint } from "../../utils/react-use";
 import { HeaderMenuList } from "../../config/menu";
+import { useArweaveContext } from "../../contexts/arconnect";
 
 const HomeHeader: FC<{ showUserInfo?: boolean }> = ({
   showUserInfo = false,
@@ -11,6 +12,7 @@ const HomeHeader: FC<{ showUserInfo?: boolean }> = ({
   const isTablet = breakpoint === "mobile";
   const [menuShow, setMenuShow] = useState<boolean>(false);
   const navigate = useNavigate();
+  const { activeAddress, connectWallet } = useArweaveContext();
 
   return (
     <div
@@ -22,25 +24,35 @@ const HomeHeader: FC<{ showUserInfo?: boolean }> = ({
       <div className="h-full flex items-center" onClick={() => navigate("/")}>
         <img src={ImgHomepage.LogoHorizonal} alt="Apus Logo" className="h-6" />
       </div>
-      {!showUserInfo ? <div
-        className={`fixed top-20 left-0 right-0 flex-1 h-screen md:h-full md:top-0 md:relative bg-[rgba(0,0,0,0.8)] md:bg-transparent`}
-        style={isTablet ? (menuShow ? {} : { display: "none" }) : {}}
-        onClick={() => {
-          setMenuShow(false)
-        }}
-      >
-        <ul className="flex-col justify-center items-center gap-12 md:h-full md:flex md:flex-row bg-[#111111] md:bg-transparent z-20">
-          {HeaderMenuList.map((item) => (
-            <Link key={item.name} to={item.path}><li className="menu-colorful" key={item.name}>
-              {item.name}
-            </li>
-            </Link>
-          ))}
-        </ul>
-      </div> : null}
+      {!showUserInfo ? (
+        <div
+          className={`fixed top-20 left-0 right-0 flex-1 h-screen md:h-full md:top-0 md:relative bg-[rgba(0,0,0,0.8)] md:bg-transparent`}
+          style={isTablet ? (menuShow ? {} : { display: "none" }) : {}}
+          onClick={() => {
+            setMenuShow(false);
+          }}
+        >
+          <ul className="flex-col justify-center items-center gap-12 md:h-full md:flex md:flex-row bg-[#111111] md:bg-transparent z-20">
+            {HeaderMenuList.map((item) => (
+              <Link key={item.name} to={item.path}>
+                <li className="menu-colorful" key={item.name}>
+                  {item.name}
+                </li>
+              </Link>
+            ))}
+          </ul>
+        </div>
+      ) : null}
 
       {!isTablet ? (
-        <div className="w-[15rem]"></div>
+        <div
+          className="btn-main btn-colorful"
+          onClick={() => {
+            activeAddress ? navigate("/console/competition/") : connectWallet();
+          }}
+        >
+          {!activeAddress ? "Connect Wallet" : "Console"}
+        </div>
       ) : (
         <img
           src={ImgHomepage.IconMenu}
