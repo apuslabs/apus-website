9a67424002c35de7a0ae5bbd2ea4f81bc9f57b1f
Jax Williams <jax@apus.network>
2024-08-14 19:53:53 +0800

    update process id

--- src/config/process.tsx
+++ src/config/process.tsx
@@ -1,2 +1,4 @@
-export const BENCHMARK_PROCESS = "xLlcWNqzvVJHOYpgFP9QV-FSF4mcmPtk8xjverzRK3U";
-export const EMBEDDING_PROCESS = "jjEkZTRO_tL4BrhMm34TLXM_wg3zoXINK6fIbp3OSUo";
+export const BENCHMARK_PROCESS = "Zv4NMG3qYgCtLLd9UTp8c0lhUjAnok2bVJahSwe1CkM";
+export const EMBEDDING_PROCESS = "0I4lfmafx816miHSJ78KFoQE9vc0pu79Czgp0VNWO84";
+// export const BENCHMARK_PROCESS = "xLlcWNqzvVJHOYpgFP9QV-FSF4mcmPtk8xjverzRK3U";
+// export const EMBEDDING_PROCESS = "jjEkZTRO_tL4BrhMm34TLXM_wg3zoXINK6fIbp3OSUo";
