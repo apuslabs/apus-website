be2789de7ac813bfb336acf86adf19e631cc85b8
Jax Williams <17276029+jax-cn@users.noreply.github.com>
2024-09-06 08:45:39 +0800

    updatge embedding process

--- src/config/process.tsx
+++ src/config/process.tsx
@@ -1,4 +1,4 @@
 export const BENCHMARK_PROCESS = "pNSXgR1gIp6zzoXZv4mfSLQfuWVzvPsLZHg8-oi_DZo";
-export const EMBEDDING_PROCESS = "0oXQyx-n3YTxe_zs1JXUQjjKQ_UveHVjnkLaU4BI9xM";
+export const EMBEDDING_PROCESS = "4pX6ABsJAJR8RWq_6tpGWfX29rFgIblvTqh6mWNKrro";
 // export const BENCHMARK_PROCESS = "xLlcWNqzvVJHOYpgFP9QV-FSF4mcmPtk8xjverzRK3U";
 // export const EMBEDDING_PROCESS = "jjEkZTRO_tL4BrhMm34TLXM_wg3zoXINK6fIbp3OSUo";
