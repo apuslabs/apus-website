5431296c7229e41c61117c3e6dc090f85d3aea17
Jax Williams <17276029+jax-cn@users.noreply.github.com>
2024-12-06 15:35:25 +0800

    give a way to update process id

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -11,4 +11,8 @@ interface NavigationMenuType {
 declare interface Window {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   ethereum: any;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  setApusContract: any;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  setMirrorContract: any;
 }
