9a5251c8a7f3cf109317a06abe6e054ba2c8d78b
Jax Williams <17276029+jax-cn@users.noreply.github.com>
2024-12-17 23:03:42 +0800

    use prod version

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -5,8 +5,8 @@ export const EMBEDDING_PROCESS = "q7QJTZbOI_avjaltnF-kP21kpL3qCs8sJSP6Wdl9wzM";
 export const CHAT_PROCESS = "4_zjOJp75rooSI6-f3lUwPJxwgcG1MXITCVqrPd_o5k";
 export const TOKEN_PROCESS = "al1xXXnWnfJD8qyZJvttVGq60z1VPGn4M5y6uCcMBUM";
 // export const AO_MINT_PROCESS = "sKd_ggG2RcLNygrnjXzpfmXWzLer77iPR8Q2dhdAw14";
-export const AO_MINT_PROCESS = "LPK-D_3gZkXtia6ywwU1wRwgFOZ-eLFRMP9pfAFRfuw";
-// export const AO_MINT_PROCESS = "1OEAToQGhSKV76oa1MFIGZ9bYxCJoxpXqtksApDdcu8";
+// export const AO_MINT_PROCESS = "LPK-D_3gZkXtia6ywwU1wRwgFOZ-eLFRMP9pfAFRfuw";
+export const AO_MINT_PROCESS = "1OEAToQGhSKV76oa1MFIGZ9bYxCJoxpXqtksApDdcu8";
 export const APUS_ADDRESS = {
   Mint: "sq_7jcV_Gide_gCXp4pYWy_c3XIcPKTO2MrD8BMneuQ",
   Mirror: "wO8Lk7vJ_XxsHXoszKOCJdVHHlku2fwkXLq0thpeNmA",
