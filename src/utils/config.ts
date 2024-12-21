e303cdb0bf0c73b8bbc2eebd92955bc9c63c06d8
Jax Williams <17276029+jax-cn@users.noreply.github.com>
2024-12-21 20:36:32 +0800

    update process id

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -8,9 +8,9 @@ export const TOKEN_PROCESS = "al1xXXnWnfJD8qyZJvttVGq60z1VPGn4M5y6uCcMBUM";
 // export const AO_MINT_PROCESS = "LPK-D_3gZkXtia6ywwU1wRwgFOZ-eLFRMP9pfAFRfuw";
 export const AO_MINT_PROCESS = "1OEAToQGhSKV76oa1MFIGZ9bYxCJoxpXqtksApDdcu8";
 export const APUS_ADDRESS = {
-  Mint: "jxMmBPi9vYs4eSxxmsyGhuY6i9JilRILkDNkqOTZE_M",
-  Mirror: "MjwyV5VEr30mQZz22CUoMk-XHx7DoUzuvZK8JslPwWg",
+  Mint: "MZdvmO8d2IdOuXxAaXw9xjOsFH6Mk2r4IeqA6cT8tyc",
+  Mirror: "3l3Qqm8YM0bDAeYv1OW3vLMat3GqEp2ieq4xYFyDtcU",
   Recipient: "U-vRZXZP3tmczr8JOW_J1wqE1KFZo3YheKF5wYBcl1Y",
 };
 export const TGE_TIME = dayjs.unix(1734973200); // 2024-12-23T17:00:00Z
-export const PRE_TGE_TIME = dayjs.unix(1734973200).subtract(3, "day"); // 2024-12-21T17:00:00Z
+export const PRE_TGE_TIME = dayjs.unix(1734973200).subtract(2, "day"); // 2024-12-21T17:00:00Z
